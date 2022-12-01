cca28991b77a0a273c0961a12e439aedea67b743
Tushar Verma <tusharverma@Tushars-MacBook-Pro.local>
2022-12-01 18:34:08 +0530

    indexing receipt

---
+++ readme.md
@@ -0,0 +1,13 @@
+<h2 align="center">
+    <samp>
+        <h1>FULLSTACK<br/>DEVELOPER</h1>
+    </samp>
+</h2>
+<p align="center">
+  <samp>
+     <strong>/** WEB3 ENTHUISIAST **/</strong>
+  </samp>    
+</p>
+<h3>
+PROJECTS:
+</h3>
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@ extern crate alloc;
 use fuel_indexer_macros::indexer;
 use fuel_indexer_plugin::types::*;
 use fuels_core::*;
+use fuel_indexer_plugin::{types::tx::*, types::Bytes32, utils::sha256_digest};
+use std::str;
+
+pub fn derive_id(id: [u8; 32], data: Vec<u8>) -> Bytes32 {
+    let mut buff: [u8; 32] = [0u8; 32];
+    let result = [id.to_vec(), data].concat();
+    buff.copy_from_slice(&sha256_digest(&result).as_bytes()[..32]);
+    Bytes32::from(buff)
+}
 
 pub fn get_zero_address() -> Address {
     Address::new([0u8; 32])
@@ -53,108 +62,95 @@ pub fn load_or_get_account(account: Address) -> Account {
 #[indexer(manifest = "/Users/tusharverma/Code/fuel-indexers/fuel-nft-block-indexer/manifest.yaml")]
 pub mod nft_indexer_module {
 
-    pub fn handle_mint_event(event: MintEvent) {
-        let MintEvent { owner, token_id } = event;
-
-        let zero_account = crate::load_or_get_account(crate::get_zero_address());
-        let owner_address = crate::get_address_from_identity(owner);
-        let owner_account = crate::load_or_get_account(owner_address);
-
-        let collection = match Collection::load(1) {
-            Some(mut c) => {
-                c.total_supply += 1;
-                c.total_minted += 1;
-                c
-            },
-            None => {
-                Collection {
-                    id: 1,
-                    contract: crate::get_zero_contract(),
-                    admin: zero_account.id,
-                    total_supply: 1,
-                    total_minted: 1,
-                }
-            }
-        };
-
-        let nft_token = match NFTToken::load(token_id) {
-            Some(token) => token,
-            None => {
-                NFTToken {
-                    id: token_id,
-                    token_id: crate::get_i64_from_u64(token_id),
-                    previous_owner: crate::get_zero_address(),
-                    current_owner: owner_address,
-                }
-            }
-        };
-
-        let mint_event = match Mint::load(token_id) {
-            Some(mint) => mint,
-            None => {
-                Mint {
-                    id: token_id,
-                    owner: owner_account.id,
-                    token: nft_token.id,
-                }
-            }
-        };
-
-        collection.save();
-        nft_token.save();
-        mint_event.save();
-    }
-
-    pub fn handle_transfer_event(event: TransferEvent) {
-        let TransferEvent { from, to, token_id } = event;
-
-        let from_address = crate::get_address_from_identity(from);
-        let to_address = crate::get_address_from_identity(to);
-        crate::load_or_get_account(from_address);
-        crate::load_or_get_account(to_address);
-
-        let nft_token = match NFTToken::load(token_id) {
-            Some(mut token) => {
-                token.previous_owner = from_address;
-                token.current_owner = to_address;
-                token
-            },
-            None => {
-                NFTToken {
-                    id: token_id,
-                    token_id: crate::get_i64_from_u64(token_id),
-                    previous_owner: from_address,
-                    current_owner: to_address,
-                }
-            }
-        };
-
-        let transfer_id = crate::get_u64_from_string_vec(
-            vec![from_address.to_string(), to_address.to_string(), token_id.to_string()]
-        );
-        let transfer = match Transfer::load(transfer_id) {
-            Some(t) => t,
-            None => {
-                Transfer {
-                    id: transfer_id,
-                    token: nft_token.id,
-                    from_user: from_address,
-                    to_user: to_address,
-                }
-            }
-        };
-
-        nft_token.save();
-        transfer.save();
-    }
-
     pub fn handle_block_event(block_data: BlockData){
-        let block = Block {
-            id: block_data.id,
-            height: block_data.height,
-            timestamp: block_data.time,
-        };
+        for tx in block_data.transactions.iter() {
+            match &tx.transaction {
+                #[allow(unused)]
+                Transaction::Script(t) => {
+                    Logger::info("Inside a script transaction. (>^‿^)>");
 
-        block.save();
+                    let gas_limit = t.gas_limit();
+                    let gas_price = t.gas_price();
+                    let maturity = t.maturity();
+                    let script = t.script();
+                    let script_data = t.script_data();
+                    let receipts_root = t.receipts_root();
+                    let inputs = t.inputs();
+                    let outputs = t.outputs();
+                    let witnesses = t.witnesses();
+                    Logger::info("Script Input");
+                    Logger::info(&format!("{:?}",script_data).to_string());
+                }
+                #[allow(unused)]
+                Transaction::Create(t) => {
+                    Logger::info("Inside a create transaction. <(^.^)>");
+
+                    let gas_limit = t.gas_limit();
+                    let gas_price = t.gas_price();
+                    let maturity = t.maturity();
+                    let bytecode_length = t.bytecode_length();
+                    let bytecode_witness_index = t.bytecode_witness_index();
+                    let inputs = t.inputs();
+                    let outputs = t.outputs();
+                    let witnesses = t.witnesses();
+                    let storage_slots = t.storage_slots();
+                    Logger::info("Transaction Input");
+                    Logger::info(&format!("{:?}",inputs[0]).to_string());
+                }
+                #[allow(unused)]
+                Transaction::Mint(t) => {
+                    Logger::info("Inside a mint transaction. <(^‿^<)");
+
+                    let tx_pointer = t.tx_pointer();
+                    let outputs = t.outputs();
+                    Logger::info("Mint Output");
+                    Logger::info(&format!("{:?}",outputs[0]).to_string());
+                }
+            }
+            for receipt in &tx.receipts {
+
+                match receipt {
+                    #[allow(unused)]
+                    Receipt::Call { id,to, param1,param2,pc,is,.. } => {
+                        Logger::info("RECEIPT CALL PARAM1");
+                        Logger::info(&format!("{:?}",id).to_string());
+                        Logger::info(&format!("{:?}",to).to_string());
+                        Logger::info(&format!("{:?}",param1).to_string());
+                        Logger::info(&format!("{:?}",param2).to_string());
+                        Logger::info(&format!("{:?}",pc).to_string());
+                        Logger::info(&format!("{:?}",is).to_string());
+                    }
+                    #[allow(unused)]
+                    Receipt::ReturnData { id,data, .. } => {
+                        Logger::info("RECEIPT RETURN DATA");
+                        Logger::info(&format!("{:?}",data).to_string());
+                    }
+                    #[allow(unused)]
+                    Receipt::Transfer {
+                        id,
+                        to,
+                        asset_id,
+                        amount,
+                        ..
+                    } => {
+                        Logger::info("RECEIPT TRANSFER AMOUNT");
+                    }
+                    #[allow(unused)]
+                    Receipt::LogData {
+                        id,data,ra,rb,pc,is,..
+                    } => {
+                        Logger::info("Inside Log Data");
+                        Logger::info(&ra.to_string());
+                        Logger::info(&rb.to_string());
+                        Logger::info(&pc.to_string());
+                        Logger::info(&is.to_string());
+                        Logger::info(&format!("{:?}",data).to_string());
+                    }
+                    _ => {
+                        Logger::info("This type is not handled yet.");
+                    }
+                }
+            }
+        }
     }
 }
